59471579f5bbfda0f6fb04f6d867125f7d23cb81
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-02 16:35:46 +0700

    Starting point for idiomatic redux

--- client/index.js
+++ client/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './modules/Todo/reducers'
-import App from './modules/Todo/components/App'
+import App from './modules/App/App'
 
 let store = createStore(todoApp)
 
 render(
-   <AppContainer>
-     <App  store={store}/>
-   </AppContainer>,
+   <Provider store={store}>
+     <App />
+   </Provider>,
    document.getElementById('app')
 )
--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -2,12 +2,16 @@ import React,{ Component } from 'react';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import TodoApp from '../Todo/components/App';
 
 class App extends Component{
   render(){
     return(
        <div>
          <Header />
+         <div className="container" style={{position:'relative',top:'70px'}}>
+           <TodoApp/>
+         </div>
          <Footer />
        </div>
     )
