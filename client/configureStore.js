19dda2d0b06328a80eb33ffa2b81a289a208dc95
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-02 23:10:07 +0700

    Applying Redux Middleware

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,46 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './modules/Todo/reducers'
 
-const logger = (store) => (next) => {
-  if(!console.group){
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state','color:grey',store.getState());
-    console.log('%c action','color:blue',action);
-    const returnValue = next(action);
-    console.log('%c next state','color:green',store.getState())
-    console.groupEnd();
-    return returnValue
-  }
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => (
-     store.dispatch = middleware(store)(store.dispatch)
-  ))
-};
-
-const promise = (store) => (next) => (action) => {
-  if(typeof action.then === 'function'){
-    return action.then(next);
-  }
-  return next(action);
-};
-
 const configureStore = () => {
-  const store = createStore(todoApp);
   const middlewares = [promise];
 
   if(process.env.NODE_ENV !== 'production'){
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
-
-  wrapDispatchWithMiddlewares(store,middlewares);
-
-  return store
+  
+  return createStore(
+     todoApp,
+     applyMiddleware(...middlewares)
+  )
 };
 
 export default configureStore
