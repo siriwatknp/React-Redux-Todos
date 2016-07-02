6d5ade331fe2ceb7c3266a5f2cb2a6020deb23b9
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-02 18:14:18 +0700

    Navigated with React Router <Link>

--- client/Root.js
+++ client/Root.js
@@ -6,10 +6,12 @@ import App from './modules/App/App'
 const Root = ({store}) => (
    <Provider store={store}>
      <Router history={browserHistory}>
-       <Route path="/" component={App} />
+       <Route path="/(:filter)" component={App} />
      </Router>
    </Provider>
 );
+
+// add () to /(:filter) to show that filter is optional
 
 
 export default Root
