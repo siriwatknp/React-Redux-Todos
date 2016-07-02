425a842629041495a7e8a2ca88019dd03ceed25e
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-02 22:21:17 +0700

    Fetching data on Route Change

--- client/modules/Todo/containers/VisibleTodoList.js
+++ client/modules/Todo/containers/VisibleTodoList.js
@@ -1,23 +1,38 @@
-
+import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 import {getVisibleTodos} from '../reducers';
+import {fetchTodos} from '../../../api';
+
+class VisibleTodoList extends Component{
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+
+  componentDidUpdate(prevProps){
+    if(this.props.filter !== prevProps.filter){
+      fetchTodos(this.props.filter).then(todos =>
+         console.log(this.props.filter, todos)
+      );
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
 
-const mapStateToProps = (state,ownProps) => ({
-  todos: getVisibleTodos(state, ownProps.filter)
-});
+const mapStateToProps = (state,ownProps) => {
+  const filter = ownProps.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  }
+};
 
-// const mapDispatchToProps = (dispatch) => ({
-//   onTodoClick(id) {
-//     dispatch(toggleTodo(id))
-//   }
-// });
 
-const VisibleTodoList = connect(
-   mapStateToProps,
-   {onTodoClick:toggleTodo}
-)(TodoList);
-
-export default VisibleTodoList
+export default connect(mapStateToProps,{onTodoClick:toggleTodo})(VisibleTodoList);
