0b56b847e5c78692496257d6828e9b731b4ade4d
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-03 21:15:36 +0700

    Avoiding race condition with thunk

--- client/modules/Todo/actions/index.js
+++ client/modules/Todo/actions/index.js
@@ -1,12 +1,16 @@
 import {v4} from 'node-uuid';
 import * as api from '../../../api';
+import {getIsFetching} from '../reducers'
 
 const requestTodos = (filter) => ({
   type: 'REQUEST_TODOS',
   filter
 });
 
-export const fetchTodos = (filter) => (dispatch) => {
+export const fetchTodos = (filter) => (dispatch, getState) => {
+  if(getIsFetching(getState(),filter)){
+    return Promise.resolve();
+  }
   dispatch(requestTodos(filter));
   return api.fetchTodos(filter).then(response =>
      dispatch(receiveTodos(filter, response))
--- client/modules/Todo/containers/VisibleTodoList.js
+++ client/modules/Todo/containers/VisibleTodoList.js
@@ -17,7 +17,7 @@ class VisibleTodoList extends Component{
 
   fetchData(){
     const {filter, fetchTodos} = this.props;
-    fetchTodos(filter);
+    fetchTodos(filter).then(() => console.log('done!'));
   }
 
   render() {
