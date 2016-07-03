04e0b52007a2fd142b8013825c5fa68604e6c37b
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-07-03 10:26:38 +0700

    Dispatching Actions Asynchronously with Thunks

--- client/modules/Todo/actions/index.js
+++ client/modules/Todo/actions/index.js
@@ -1,15 +1,18 @@
 import {v4} from 'node-uuid';
 import * as api from '../../../api';
 
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
   type: 'REQUEST_TODOS',
   filter
 });
 
-export const fetchTodos = (filter) =>
-  api.fetchTodos(filter).then(response =>
-    receiveTodos(filter, response)
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter));
+  return api.fetchTodos(filter).then(response =>
+     dispatch(receiveTodos(filter, response))
   );
+};
+  
 
 const receiveTodos = (filter,response) => ({
   type: 'RECEIVE_TODOS',
